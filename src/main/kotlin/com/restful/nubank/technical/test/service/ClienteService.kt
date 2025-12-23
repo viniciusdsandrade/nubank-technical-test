1278d4dfccf96c7dcb72163831117a9025efe00c
andrade <viniciusdsandrade0662@gmail.com>
2025-12-23 13:46:16 -0300

    fix:

--- src/main/kotlin/com/restful/nubank/technical/test/service/ClienteService.kt
+++ src/main/kotlin/com/restful/nubank/technical/test/service/ClienteService.kt
@@ -1,12 +1,15 @@
 package com.restful.nubank.technical.test.service
 
 import com.restful.nubank.technical.test.entity.Cliente
+import org.springframework.transaction.annotation.Transactional
 
 interface ClienteService {
     @Transactional
     fun criar(nome: String, email: String): Cliente
+
     @Transactional(readOnly = true)
     fun listarTodosComContatos(): List<Cliente>
+
     @Transactional(readOnly = true)
     fun buscarPorId(id: Long): Cliente
 }
--- src/main/kotlin/com/restful/nubank/technical/test/service/ContatoService.kt
+++ src/main/kotlin/com/restful/nubank/technical/test/service/ContatoService.kt
@@ -4,8 +4,10 @@ import com.restful.nubank.technical.test.entity.Contato
 import org.springframework.transaction.annotation.Transactional
 
 interface ContatoService {
+
     @Transactional(readOnly = true)
     fun listarPorCliente(clienteId: Long): List<Contato>
+
     @Transactional
     fun criar(clienteId: Long, tipo: String, valor: String): Contato
 }
