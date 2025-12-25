aff5706343f7e34ac4af219a01facf93a0fb71a9
andrade <viniciusdsandrade0662@gmail.com>
2025-12-25 12:32:53 -0300

    refactor:

--- src/main/kotlin/com/restful/nubank/technical/test/exception/BusinessRuleException.kt
+++ src/main/kotlin/com/restful/nubank/technical/test/exception/BusinessRuleException.kt
@@ -1,3 +1,7 @@
 package com.restful.nubank.technical.test.exception
 
+import org.springframework.http.HttpStatus.CREATED
+import org.springframework.web.bind.annotation.ResponseStatus
+
+@ResponseStatus(CREATED)
 class BusinessRuleException(message: String) : RuntimeException(message)
--- src/main/kotlin/com/restful/nubank/technical/test/exception/ResourceNotFoundException.kt
+++ src/main/kotlin/com/restful/nubank/technical/test/exception/ResourceNotFoundException.kt
@@ -1,3 +1,7 @@
 package com.restful.nubank.technical.test.exception
 
+import org.springframework.http.HttpStatus.NOT_FOUND
+import org.springframework.web.bind.annotation.ResponseStatus
+
+@ResponseStatus(NOT_FOUND)
 class ResourceNotFoundException(message: String) : RuntimeException(message)
